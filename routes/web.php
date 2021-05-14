8e6b6b51aa3bd8a9cc62180f6b73abe26595dec4
hith-hj <hjhaithem@gmail.com>
2021-05-14 17:00:00 +0300

    se

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Livewire\Showitem;
-use App\Http\Livewire\Body;
-
 
 use App\Models\Item;
 require __DIR__.'/auth.php';
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,22 +21,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
-
 Route::get('/home', function () {
     return view('home');
 })->middleware(['auth'])->name('home');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::view('/about','about')->name('about');
 
-// Route::post('/addItem','HomeController@addItem')->name('addItem');
 Route::post('/addItem',[HomeController::class,'addItem'])->name('addItem');
+
 Route::get('/item/show/&{id}&/HtybVertnXAsdR',function($id){
-    // dd($id);
     $feed = Item::find($id);
     if(isset($feed)){
         $feed->views +=1 ;
@@ -49,9 +40,5 @@ Route::get('/item/show/&{id}&/HtybVertnXAsdR',function($id){
     }
     return view('show',compact('feed'));
 })->name('showItem');
-// Route::get('/item/show/&{id}',[Body::class,'viewItem']);
 
-Route::get('/show/{id}', Showitem::class
-    // return view('livewire.showitem',['g_id',$id])->extends('layouts.sapp')->section('content');
-);
 
