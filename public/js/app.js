67329f09fbc70e17250aa41992014d490dc56f95
hith-hj <hjhaithem@gmail.com>
2021-05-16 01:24:01 +0300

    59

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,18 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../sw.js')
+            .then(serviceWorker => {
+                console.log("Service Worker registered");
+            })
+            .catch(error => {
+                console.error("Error registering the Service Worker: ", error);
+            });
+    })
+} else {
+    console.log('wtf');
+}
+
 // Livewire
 
 Livewire.on('notifi', (e) => {
