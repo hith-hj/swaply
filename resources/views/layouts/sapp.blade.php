0cd905ba71396f07df5280035883a23872c1495a
hith-hj <hjhaithem@gmail.com>
2021-05-25 21:50:11 +0300

    897

--- resources/views/layouts/sapp.blade.php
+++ resources/views/layouts/sapp.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="theme-color" content="#9decae">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">     
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">     
         <meta name="description" content="a site for swaping items freely or almost"> 
         @auth
             <meta name="logged" content="{{Auth::user()->name}}">
